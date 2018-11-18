faca3e9292fda07815364896188205941cd09e6b
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-18 17:26:33 -0500

    Initial commit

---
+++ phoneBook.py
@@ -0,0 +1,16 @@
+# $ python phonebook.py
+# Electronic Phone Book
+# =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit
+# What do you want to do (1-5)?
+# If they choose to look up an entry, you will ask them for the person's name, and then look up the person's phone number by the given name and print it to the screen.
+# If they choose to set an entry, you will prompt them for the person's name and the person's phone number,
+# If they choose to delete an entry, you will prompt them for the person's name and delete the given person's entry.
+# If they choose to list all entries, you will go through all entries in the dictionary and print each out to the terminal.
+# Search for an entry
+# You can limit this to Name, or loop through the entire dictionary
+# If they choose to quit, end the program.
