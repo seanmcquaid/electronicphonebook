1a021cee0a72616f142ed2552051f9197b072bb3
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-18 20:49:13 -0500

    Final version complete

--- phoneBook.py
+++ phoneBook.py
@@ -1,16 +1,34 @@
-# $ python phonebook.py
-# Electronic Phone Book
-# =====================
-# 1. Look up an entry
-# 2. Set an entry
-# 3. Delete an entry
-# 4. List all entries
-# 5. Quit
-# What do you want to do (1-5)?
-# If they choose to look up an entry, you will ask them for the person's name, and then look up the person's phone number by the given name and print it to the screen.
-# If they choose to set an entry, you will prompt them for the person's name and the person's phone number,
-# If they choose to delete an entry, you will prompt them for the person's name and delete the given person's entry.
-# If they choose to list all entries, you will go through all entries in the dictionary and print each out to the terminal.
-# Search for an entry
-# You can limit this to Name, or loop through the entire dictionary
-# If they choose to quit, end the program.
+cont = True
+
+phoneBook = {}
+
+while(cont == True):
+    print "Electric Phone Book"
+    print "===================="
+    print "1. Look up a contact"
+    print "2. Create a new contact"
+    print "3. Delete a contact"
+    print "4. List all contacts"
+    print "5. Quit"
+    option = raw_input("Which option would you like to do? Choose a number between 1 and 5:  ")
+    
+    if (option == 1):
+        searchContact = raw_input("Whose number are you looking for?  ").upper()
+        if (searchContact) in phoneBook.keys():
+            print "Their number is " + phoneBook[searchContact]
+        else:
+            print "Number not found, please try again!"
+    elif (option == "2"):
+        contactName = raw_input("What's their name?  ").upper()
+        contactNum = raw_input("What's their phone number? Please use the following format: xxx-xxx-xxxx:  ")
+        phoneBook[contactName] = contactNum
+    elif (option == "3"):
+        deleteContact = raw_input("Who do you want to delete?  ").upper()
+        del phoneBook[deleteContact]
+    elif (option == "4"):
+        for (k,v) in phoneBook.items():
+            print (k,v)
+    elif (option == "5"):
+        cont = False
+    else:
+        print "Option not found, please select a number between 1 and 5"
