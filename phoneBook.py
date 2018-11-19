80c3ea61ac9f9c18d6a99f3cde8d1f699b2a89a2
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-19 09:09:10 -0500

    Fixed identation

--- phoneBook.py
+++ phoneBook.py
@@ -10,25 +10,25 @@ while(cont == True):
 3. Delete a contact
 4. List all contacts
 5. Quit"""
-    option = raw_input("Which option would you like to do? Choose a number between 1 and 5:  ")
+option = raw_input("Which option would you like to do? Choose a number between 1 and 5:  ")
     
-    if (option == "1"):
-        searchContact = raw_input("Whose number are you looking for?  ").upper()
-        if (searchContact) in phoneBook.keys():
-            print "Their number is " + phoneBook[searchContact]
-        else:
-            print "Number not found, please try again!"
-    elif (option == "2"):
-        contactName = raw_input("What's their name?  ").upper()
-        contactNum = raw_input("What's their phone number? Please use the following format: xxx-xxx-xxxx:  ")
-        phoneBook[contactName] = contactNum
-    elif (option == "3"):
-        deleteContact = raw_input("Who do you want to delete?  ").upper()
-        del phoneBook[deleteContact]
-    elif (option == "4"):
-        for (k,v) in phoneBook.items():
-            print (k,v)
-    elif (option == "5"):
-        cont = False
+if (option == "1"):
+    searchContact = raw_input("Whose number are you looking for?  ").upper()
+    if (searchContact) in phoneBook.keys():
+        print "Their number is " + phoneBook[searchContact]
     else:
-        print "Option not found, please select a number between 1 and 5"
+        print "Number not found, please try again!"
+elif (option == "2"):
+    contactName = raw_input("What's their name?  ").upper()
+    contactNum = raw_input("What's their phone number? Please use the following format: xxx-xxx-xxxx:  ")
+    phoneBook[contactName] = contactNum
+elif (option == "3"):
+    deleteContact = raw_input("Who do you want to delete?  ").upper()
+    del phoneBook[deleteContact]
+elif (option == "4"):
+    for (k,v) in phoneBook.items():
+        print (k,v)
+elif (option == "5"):
+        cont = False
+else:
+    print "Option not found, please select a number between 1 and 5"
